b505799e4cd2a03962e360a583452a0953535896
PeterKelemen2 <peter.kelemen456@gmail.com>
2026-03-06 19:05:18 +0100

    Initial commit

---
+++ obd2_reader/src/lib.rs
@@ -0,0 +1,19 @@
+mod command;
+mod connection;
+mod pid;
+mod response;
+
+pub fn add(left: u64, right: u64) -> u64 {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
