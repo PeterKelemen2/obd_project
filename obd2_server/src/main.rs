bcfa73fb41622442456c1b2beea762699dd3d204
PeterKelemen2 <peter.kelemen456@gmail.com>
2026-03-06 19:59:54 +0100

    Add raw response for testing

--- obd2_server/src/main.rs
+++ obd2_server/src/main.rs
@@ -7,9 +7,14 @@ fn read_live_data(port: &str) -> Result<(), ObdError> {
     println!("RPM:   {:.0}", obd.query_engine_rpm()?);
     println!("Speed: {} km/h", obd.query_vehicle_speed()?);
     println!("Temp:  {} °C", obd.query_coolant_temp()?);
+
+    let raw_response = obd.send_raw("0111\r")?;
+    println!("Raw: {}", raw_response);
+
     Ok(())
 }
 
-fn main() {
-    read_live_data("/dev/rfcomm1").unwrap();
+fn main() -> Result<(), ObdError> {
+    read_live_data("/dev/rfcomm1")?;
+    Ok(())
 }
